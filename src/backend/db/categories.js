8c7f19f57e4b0c2f50ddb6687ead88badd197b66
SHILPE SAXENA <shilpecsaxena9098@gmail.com>
2022-03-21 10:50:17 +0530

    styles: host images to postimage

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -4,28 +4,28 @@ export const categories = [
   {
     _id: uuid(),
     categoryName: "Fiction",
-    categoryImage: require("../../assets/fiction-book1.jpg"),
+    categoryImage: "https://i.postimg.cc/rwnYHtvB/fiction-book1.jpg",
     description:
       "literature in the form of prose, especially novels, that describes imaginary events and people",
   },
   {
     _id: uuid(),
     categoryName: "Horror",
-    categoryImage: require("../../assets/horror00.jpg"),
+    categoryImage: "https://i.postimg.cc/2j0vhFhN/horror00.jpg",
     description:
       "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
   },
   {
     _id: uuid(),
     categoryName: "Children & Teens",
-    categoryImage: require("../../assets/childteens03.jpg"),
+    categoryImage: "https://i.postimg.cc/qqFzYwxt/childteens03.jpg",
     description:
       "Children's literature or juvenile literature includes stories, books, magazines, and poems that are created for children.",
   },
   {
     _id: uuid(),
     categoryName: "Personal Development",
-    categoryImage: require("../../assets/personalD01.jpg"),
+    categoryImage: "https://i.postimg.cc/1zS4LfwK/personal-D01.jpg",
     description:
       "The goal of personal development books is to provide information, insights, and instructions for improving your life and ways of thinking.",
   },
