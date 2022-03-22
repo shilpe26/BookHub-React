14f81931057265cfbac36f9acdfe7e8f7099d462
SHILPE SAXENA <shilpecsaxena9098@gmail.com>
2022-03-22 13:16:18 +0530

    refactor: code of product files

--- src/pages/product/card/Card.jsx
+++ src/pages/product/card/Card.jsx
@@ -1,111 +1,56 @@
-import React from 'react'
+import {React,useState, useEffect} from 'react';
+import axios from "axios";
 import "../../../App.css";
 function Card({drawerVisible, setDrawerVisible}) {
+    const [products, setProducts] = useState([]);
+    
+    useEffect(() => {
+        (async () => {
+            try {
+                const response = await axios.get('/api/products');
+                setProducts(response.data.products);
+              } catch (error) {
+                console.error(error);
+              }
+        })();
+    },[]);
+
   return (
     
         <section className="cards_for-book">
-            <div className="card-vertical flex-items">
+            {products.length === 0 ? <h3>Products Loading...</h3> : 
+            products.map(({_id,
+                title,
+                author,
+                price,
+                productImage,
+                inStock,
+                rating,
+                isFastDelivery,
+                categoryName}) =>
+                <div key={_id} className="card-vertical flex-items">
                 <div className="image-container badge-container">
-                    <img className="img-responsive card-img" src={require("../../../assets/fiction-book2.jpg")} alt="fiction_books"/>
+                    <img className="img-responsive card-img" src={productImage} alt="fiction_books"/>
                     <span className="badge bg-primary">#1Bestseller</span>
                 </div>
                 <div className="text-container">
-                    <h3 className="price-tag">$80</h3>
-                    <h4 className="heading-primary">The Queen of Nothing</h4>
-                    <p className="sub-heading">by Holly Black</p>
+                    <h3 className="price-tag">{price}</h3>
+                    <h4 className="heading-primary">{title}</h4>
+                    <p className="sub-heading">by {author}</p>
+                    <h3 className="price-tag">{rating}⭐</h3>
                     <div className="buttons">
                         <button type="button">Add to Cart</button>
                     </div>
                 </div>
-            </div>
-
-            <div className="card-vertical flex-items">
-                <div className="image-container badge-container">
-                    <img className="img-responsive card-img" src={require("../../../assets/fiction-book3.jpg")} alt="fiction_books"/>
-                    <span className="badge bg-primary">#1Bestseller</span>
                 </div>
-                <div className="text-container">
-                    <h3 className="price-tag">$22.10</h3>
-                    <h4 className="heading-primary">A Trace of Poison</h4>
-                    <p className="sub-heading">by Colleen Cambridge</p>
-
-                    <div className="buttons">
-                        <button type="button">Add to Cart</button>
-                    </div>
-                </div>
-            </div>
-
-            <div className="card-vertical flex-items">
-                <div className="image-container badge-container">
-                    <img className="img-responsive card-img" src={require("../../../assets/fiction-book4.jpg")} alt="fiction_books"/>
-                    <span className="badge bg-primary">#1Bestseller</span>
-                </div>
-                <div className="text-container">
-                    <h3 className="price-tag">$16.99</h3>
-                    <h4 className="heading-primary">Apoca Lips</h4>
-                    <p className="sub-heading">by Piers Anthony</p>
-
-                    <div className="buttons">
-                        <button type="button">Add to Cart</button>
-                    </div>
-                </div>
-            </div>
-
-            <div className="card-vertical flex-items">
-                <div className="image-container badge-container">
-                    <img className="img-responsive card-img" src={require("../../../assets/fiction-book5.jpg")} alt="fiction_books"/>
-                    <span className="badge bg-primary">#1Bestseller</span>
-                </div>
-                <div className="text-container">
-                    <h3 className="price-tag">$4.99</h3>
-                    <h4 className="heading-primary">Imaginary Friends</h4>
-                    <p className="sub-heading">by Arlene F. Marks</p>
-
-                    <div className="buttons">
-                        <button type="button">Add to Cart</button>
-                    </div>
-                </div>
-            </div>
-
-            <div className="card-vertical flex-items">
-                <div className="image-container badge-container">
-                    <img className="img-responsive card-img" src={require("../../../assets/fiction-book6.jpg")} alt="fiction_books"/>
-                    <span className="badge bg-primary">#1Bestseller</span>
-                </div>
-                <div className="text-container">
-                    <h3 className="price-tag">$18.99</h3>
-                    <h4 className="heading-primary">After the Revolution
-                    </h4>
-                    <p className="sub-heading">by Robert Evans</p>
-
-                    <div className="buttons">
-                        <button type="button">Add to Cart</button>
-                    </div>
-                </div>
-            </div>
-
-            <div className="card-vertical flex-items">
-                <div className="image-container badge-container">
-                    <img className="img-responsive card-img" src={require("../../../assets/fiction-book7.jpg")} alt="fiction_books"/>
-                    <span className="badge bg-primary">#1Bestseller</span>
-                </div>
-                <div className="text-container">
-                    <h3 className="price-tag">$9.49</h3>
-                    <h4 className="heading-primary">The Measure of a Man
-                    </h4>
-                    <p className="sub-heading">by William W. Johnstone</p>
-
-                    <div className="buttons">
-                        <button type="button">Add to Cart</button>
-                    </div>
-                </div>
-            </div>
-
+                )
+            }
             <div className="filter_btn-book">
                 <button onClick={() => setDrawerVisible(visible => !visible)} type="button" className="btn_for-bookhub">
                     {drawerVisible ? <i className="fas fa-filter"></i> : <i className="fas fa-xmark"></i> }
                     </button>
             </div>
+            
         </section>
   )
 }
