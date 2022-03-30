6d373021e1401d2f033e10ef313b10bf393b55ba
SHILPE SAXENA <shilpecsaxena9098@gmail.com>
2022-03-30 21:56:53 +0530

    add: Login & Signup Pages styles

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Login() {
-	return <div>Login</div>;
+	return (
+		<div className="login-form-container">
+			<form className="login_form-bookhub" action="">
+				<h3 className="heading-login">Login</h3>
+				<input
+					type="email"
+					className="box"
+					placeholder="Email"
+					aria-label="true"
+				/>
+				<input
+					type="password"
+					className="box"
+					placeholder="Password"
+					aria-label="true"
+				/>
+				<div className="checkbox">
+					<input type="checkbox" id="remember-me" aria-label="true" />
+					<label className="label-bookhub" htmlFor="remember-me">
+						remember me
+					</label>
+				</div>
+				<input type="submit" value="Login" className="btn" aria-label="true" />
+				<p className="additional-desc">forget password ? Click here</p>
+				<p className="additional-desc">
+					don't have an account ?
+					<Link to="/signup">
+						<button type="button" className="btn-warning">
+							create one
+						</button>
+					</Link>
+				</p>
+			</form>
+		</div>
+	);
 }
 
 export { Login };
--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,9 +1,56 @@
-import React from 'react';
-
+import React from "react";
+import { Link } from "react-router-dom";
 function Signup() {
-  return (
-    <div>Signup</div>
-  )
+	return (
+		<div className="login-form-container signup">
+			<form className="login_form-bookhub" action="">
+				<h3 className="heading-login">SIGN UP</h3>
+				<input
+					type="email"
+					name=""
+					className="box"
+					placeholder="Email"
+					aria-label="true"
+				/>
+				<input
+					type="password"
+					name=""
+					className="box"
+					placeholder="First name"
+					aria-label="true"
+				/>
+				<input
+					type="password"
+					name=""
+					className="box"
+					placeholder="Last name"
+					aria-label="true"
+				/>
+				<input
+					type="password"
+					name=""
+					className="box"
+					placeholder="Password"
+					aria-label="true"
+				/>
+				<input
+					type="password"
+					name=""
+					className="box"
+					placeholder="Confirm password"
+					aria-label="true"
+				/>
+				<Link to="/">
+					<input
+						type="submit"
+						value="sign up"
+						className="btn"
+						aria-label="true"
+					/>
+				</Link>
+			</form>
+		</div>
+	);
 }
 
-export {Signup}
+export { Signup };
