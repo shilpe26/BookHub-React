ff58e01f5c91825a6ed71c9882cf6047663b9f72
SHILPE SAXENA <shilpecsaxena9098@gmail.com>
2022-04-02 12:42:05 +0530

    fix: unwanted lines

--- src/pages/filters/Product-context.jsx
+++ src/pages/filters/Product-context.jsx
@@ -1,6 +1,19 @@
-import { createContext, useContext, useReducer, useState, useEffect } from "react";
+import {
+	createContext,
+	useContext,
+	useReducer,
+	useState,
+	useEffect,
+} from "react";
 import axios from "axios";
-import { fastDelivery, inStock, priceRange, sortByPrice, rating, category} from "./filters";
+import {
+	fastDelivery,
+	inStock,
+	priceRange,
+	sortByPrice,
+	rating,
+	category,
+} from "./filters";
 import { getProducts } from "./getProducts";
 import { productFun } from "./productFun";
 import { initialFilters } from "./initialFilters";
@@ -9,38 +22,36 @@ const ProductContext = createContext();
 
 const useProduct = () => useContext(ProductContext);
 
-const ProductProvider = ({ children }) => {  
+const ProductProvider = ({ children }) => {
+	const [state, dispatch] = useReducer(productFun, initialFilters);
 
-  const [state, dispatch] = useReducer(productFun, initialFilters);
+	const [products, setProducts] = useState([]);
 
-  const [products, setProducts] = useState([]);
-    
-    useEffect(() => {
-        (async () => {
-            try {
-                const response = await axios.get('/api/products');
-                setProducts(response.data.products);
-              } catch (error) {
-                console.error(error);
-              }
-        })();
-    },[]);
+	useEffect(() => {
+		(async () => {
+			try {
+				const response = await axios.get("/api/products");
+				setProducts(response.data.products);
+			} catch (error) {
+				console.error(error);
+			}
+		})();
+	}, []);
 
-    const filteredProducts = getProducts(
-      sortByPrice,
-      inStock,
-      fastDelivery,
-      priceRange,
-      rating,
-      category
-    )(state, [...products]);
-    console.log(filteredProducts);
-      
-  return (
-    <ProductContext.Provider value={{ filteredProducts, state, dispatch }}>
-      {children}
-    </ProductContext.Provider>
-  );
+	const filteredProducts = getProducts(
+		sortByPrice,
+		inStock,
+		fastDelivery,
+		priceRange,
+		rating,
+		category
+	)(state, [...products]);
+
+	return (
+		<ProductContext.Provider value={{ filteredProducts, state, dispatch }}>
+			{children}
+		</ProductContext.Provider>
+	);
 };
 
 export { ProductProvider, useProduct };
