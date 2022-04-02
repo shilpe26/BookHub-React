a14049f3fca67b59d8e130c42cdded5afa0014f4
SHILPE SAXENA <shilpecsaxena9098@gmail.com>
2022-04-02 12:59:22 +0530

    fix: foter link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,20 +9,26 @@ function Footer() {
 						href="https://twitter.com/ShilpeSaxena"
 						target="_blank"
 						rel="noreferrer"
-						className="fab fa-twitter footer_for-bookhub"
-					></a>
+						className="footer_for-bookhub"
+					>
+						<i className="fab fa-twitter"></i>
+					</a>
 					<a
 						href="https://www.linkedin.com/in/shilpe-saxena-heartly-winner/"
 						target="_blank"
 						rel="noreferrer"
-						className="fab fa-linkedin footer_for-bookhub"
-					></a>
+						className="footer_for-bookhub"
+					>
+						<i className="fab fa-linkedin"></i>
+					</a>
 					<a
 						href="https://github.com/shilpe26"
 						target="_blank"
 						rel="noreferrer"
-						className="fab fa-github footer_for-bookhub"
-					></a>
+						className="footer_for-bookhub"
+					>
+						<i className="fab fa-github"></i>
+					</a>
 				</div>
 				<div className="credit">
 					Made with 💙 by<span> Shilpe Saxena</span>
