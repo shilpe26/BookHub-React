65cb6b8903bee029a7cb56bad68056796bb2b423
SHILPE SAXENA <shilpecsaxena9098@gmail.com>
2022-06-18 14:53:08 +0530

    feat: add authentication

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import { Navbar, Footer } from "./components/Component";
+import { Navbar, Footer, HideAuth, RequiresAuth } from "./components/Component";
 import { Home, Cart, Product, Wishlist, Login, Signup } from "./pages/Pages";
 import Mockman from "mockman-js";
 import { NotFound } from "./pages/page_not_found/NotFound";
 import { ProductDetail } from "./pages/ProductDetail/ProductDetail";
-
+import "./styles/utility.css";
 function App() {
 	return (
 		<div className="App">
@@ -14,14 +14,42 @@ function App() {
 			</div>
 			<div className="main-style">
 				<Routes>
-					<Route path="/" element={<Home />}></Route>
-					<Route path="/login" element={<Login />}></Route>
-					<Route path="/signup" element={<Signup />}></Route>
-					<Route path="/cart" element={<Cart />}></Route>
-					<Route path="/Product" element={<Product />}></Route>
-					<Route path="/Product/:productId" element={<ProductDetail />}></Route>
-					<Route path="/wishlist" element={<Wishlist />}></Route>
-					<Route path="/mock" element={<Mockman />}></Route>
+					<Route path="/" element={<Home />} />
+					<Route
+						path="/login"
+						element={
+							<HideAuth>
+								<Login />
+							</HideAuth>
+						}
+					/>
+					<Route
+						path="/signup"
+						element={
+							<HideAuth>
+								<Signup />
+							</HideAuth>
+						}
+					/>
+					<Route
+						path="/cart"
+						element={
+							<RequiresAuth>
+								<Cart />
+							</RequiresAuth>
+						}
+					/>
+					<Route path="/Product" element={<Product />} />
+					<Route path="/Product/:productId" element={<ProductDetail />} />
+					<Route
+						path="/wishlist"
+						element={
+							<RequiresAuth>
+								<Wishlist />
+							</RequiresAuth>
+						}
+					/>
+					<Route path="/mock" element={<Mockman />} />
 					<Route path="*" element={<NotFound />} />
 				</Routes>
 			</div>
---
+++ src/Context/authReducerFunction.jsx
@@ -0,0 +1,34 @@
+import { initialAuthData } from "./initialAuthData";
+
+export function authReducerFunction(authState, action) {
+	switch (action.type) {
+		case "EMAIL":
+			return { ...authState, email: action.payload };
+		case "PASSWORD":
+			return { ...authState, password: action.payload };
+		case "NAME":
+			return { ...authState, name: action.payload };
+		case "USER-DATA":
+			const { encodedToken } = action.payload;
+			return { ...authState, encodedToken };
+		case "RESET-DATA":
+			return { ...initialAuthData };
+		case "RESET-FORM":
+			return { ...authState, email: "", password: "", name: "" };
+		case "ERROR":
+			return { ...authState, errorMessage: action.payload };
+		case "TEST-CREDENTIALS":
+			return {
+				...authState,
+				email: "adarshbalika@gmail.com",
+				password: "adarshBalika123",
+			};
+		case "LOCAL-STORAGE":
+			return {
+				...authState,
+				encodedToken: localStorage.getItem("userToken"),
+			};
+		default:
+			return authState;
+	}
+}
